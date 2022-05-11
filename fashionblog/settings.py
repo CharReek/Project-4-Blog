b35f57f5a7632317fc9f60975cca8c8081bb9ea3
CharReek <charlottereek@outlook.com>
2022-05-11 08:40:31 +0000

    initial deploy update

--- fashionblog/settings.py
+++ fashionblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fashionblog.heroku.com', 'localhost']
+ALLOWED_HOSTS = ["project4-blog-creek.herokuapp.com", "localhost"]
 
 
 # Application definition
