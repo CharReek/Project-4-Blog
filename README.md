9b629e71671bb06869701003c588aca0e2414833
CharReek <charlottereek@outlook.com>
2022-05-12 12:29:45 +0000

    make styling changes to home page

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 colour generator - coolors
 
 blog inspirations - hello fashion/ atlantic - pacific 
+
+credits - 
+code instutute walk through for installing all auth 
+https://djangocentral.com/building-a-blog-application-with-django/ for settign up views 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -11,7 +12,7 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
-    
+
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
@@ -19,15 +20,16 @@ class PostDetail(View):
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-            
+
         return render(
             request,
             "post_detail.html",
-            
+
             {
-                "post": post, 
-                "comments": comments, 
-                "liked": liked
-            
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm,
+
             },
         )
