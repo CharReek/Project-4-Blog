4493f6fd3e908f3d00a74c8f25321df9d4bdaae9
CharReek <charlottereek@outlook.com>
2022-05-17 09:41:18 +0000

    add admin section for lookbook posts

--- lookbook/admin.py
+++ lookbook/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Post, Comment, Image
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_on')
+    list_filter = ('name', 'created_on')
+    search_fields = ('name', 'created_on')
+
