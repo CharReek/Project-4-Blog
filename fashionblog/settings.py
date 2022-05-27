46482f27a1efe41d9dd2e2788b13547451f166ad
CharReek <charlottereek@outlook.com>
2022-05-27 15:55:21 +0000

    change debug to false

--- fashionblog/settings.py
+++ fashionblog/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage' 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE ='cloudinary_storage.storage.StaticHashedCloudinaryStorage'
